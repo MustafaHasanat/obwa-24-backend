77d9debffeee3b0ec0e6315b4503ac75a81616a0
MustafaHasanat <mustafa.hasanat99@gmail.com>
2025-08-28 11:35:58 +0300

    feat: new commit

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,5 +1,5 @@
 export const corsHeaders = {
-  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Origin": "*, http://localhost:3003, http://localhost:3000, capacitor://localhost",
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type, Authorization",
 };
