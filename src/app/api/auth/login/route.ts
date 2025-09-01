cb89172674f00358f7565e0c9dfd25724cb09318
MustafaHasanat <mustafa.hasanat99@gmail.com>
2025-09-01 15:19:51 +0300

    feat: new commit

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -12,7 +12,7 @@ export async function OPTIONS(request: NextRequest) {
   return NextResponse.json({}, { headers: getCorsHeaders(origin) });
 }
 
-export async function GET(
+export async function POST(
   request: NextRequest
 ): Promise<NextResponse<CustomResponse<User>>> {
   try {
